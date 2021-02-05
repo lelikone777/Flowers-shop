a94c346af19e73464a6f80f5d6cc151409465584
KALININ ALEXEY <2257855@gmail.com>
2021-02-05 21:35:58 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const flowersSlider = new Swiper(".flowers-slider", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
