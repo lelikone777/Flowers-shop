f36744a21e9c7b7a8a920da0ca0ff32523762700
KALININ ALEXEY <2257855@gmail.com>
2021-02-06 02:08:23 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,60 @@
-const flowersSlider = new Swiper(".flowers-slider", {
+$(document).ready(function () {
+  const flowersSlider = new Swiper(".flowers-slider", {
+    loop: true,
+    slidesPerView: 6,
 
-  loop: true,
-  slidesPerView: 6,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  }
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+      },
+      480: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 6,
+      },
+    },
+  });
 
-});
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    loop: true,
+    slidesPerView: 1,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  loop: true,
-  slidesPerView: 1,
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  $("#review-1").on("click", function () {
+    $.fancybox.open(
+      [
+        {
+          src: "https://source.unsplash.com/IvfoDk30JnI/1500x1000",
+          opts: {
+            caption: "First caption",
+            thumb: "https://source.unsplash.com/IvfoDk30JnI/240x160",
+          },
+        },
+        {
+          src: "https://source.unsplash.com/0JYgd2QuMfw/1500x1000",
+          opts: {
+            caption: "Second caption",
+            thumb: "https://source.unsplash.com/0JYgd2QuMfw/240x160",
+          },
+        },
+      ],
+      {
+        loop: true,
+        thumbs: {
+          autoStart: true,
+        },
+      }
+    );
+  });
 });
