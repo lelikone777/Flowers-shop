9c9e0d40d456a3b686da684b620913c79843fd88
KALININ ALEXEY <56075795+lelikone777@users.noreply.github.com>
2021-02-05 12:45:17 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const flowersSlider = new Swiper(".flowers-slider", {
+
+  loop: true,
+  slidesPerView: 6,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+
+});
+
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  loop: true,
+  slidesPerView: 6,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
